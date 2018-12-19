6ae43aaef5ebd7157fba477d307702ee461f25e9
Marcelo Martin <marcelo18a@msn.com>
2018-12-19 04:06:08 -0300

    Initial state with image loaded

---
+++ app/assets/javascripts/games.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+	const gameImg = document.querySelector('.game-img');
+
+	gameImg.addEventListener('click', function(e) {
+		console.log("jojojojo")
+		console.log(e.target)
+	})
+
+	console.log("lalalala")
+
+}
